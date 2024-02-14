4136f11333658e9ea66c60c925d5b4933932dcf0
tammyw25 <tammy.wong25@ycdsbk12.ca>
2024-02-14 11:33:59 -0500

    UpdatedProgram1

--- Program1.java
+++ Program1.java
@@ -1,13 +1,26 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that takes two sides of a right-angle triagnle from the user 
+  * and calculates the hypotenuse
+  * @author: W. Tammy
   */
   
   public void run() {
-    // your code goes here
+    // Declare variables
+    double dblSideA;
+    double dblSideB;
+    double dblHypotenuse;
+
     
+    // Get lenghts of side A and side B from user 
+    dblSideA = readDouble("Enter the length of side A: ");
+    dblSideB = readDouble("Enter the length of Side B: ");
+
+    // Calculate hyoptenuse: c = sqrt(a^2 + b^2)\
+    dblHypotenuse = Math.sqrt(Math.pow(dblSideA, 2) + Math.pow(dblSideB, 2));
     
+    // Output results
+    System.out.println("The hypotenuse of this right-angle triangle is " + dblHypotenuse);
   }
 }
